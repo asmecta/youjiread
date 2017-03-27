9ca246481a6b27975db3f78caae2945ca91fd4c9
aSmecta <jiaoxiangpo2007@live.cn>
2017-03-28 00:01:36 +0800

    补充文件

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by Administrator on 2017/3/26.
  */
+'use trict'
+var b =document.getElementsByClassName('search')[0];
+b.addEventListener('click',function () {
+    var a = document.getElementById('sBar');
+    if (a.className == "hidden"){
+        a.className = "";
+        b.className = "search sel";
+    }else {
+        a.className = a.className + "hidden";
+        b.className = "search default";
+    }
+},false);
