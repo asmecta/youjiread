e52387be68d971248be97ed4af91ec2a3813ca54
aSmecta <jiaoxiangpo2007@live.cn>
2017-03-28 23:49:43 +0800

    列表功能完成

--- src/app.js
+++ src/app.js
@@ -6,10 +6,25 @@ var b =document.getElementsByClassName('search')[0];
 b.addEventListener('click',function () {
     var a = document.getElementById('sBar');
     if (a.className == "hidden"){
-        a.className = "";
+        a.className = "container";
         b.className = "search sel";
     }else {
-        a.className = a.className + "hidden";
+        a.className = "hidden";
         b.className = "search default";
     }
 },false);
+var xmlhttp = new XMLHttpRequest();
+var books = document.getElementById("book");
+xmlhttp.onreadystatechange=function()
+{
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+        var book = eval ("(" + xmlhttp.responseText + ")");
+        for (var i = 0; i<book.length;i++){
+            var mediaHtml ='<div class="media"><div class="media-left"><a href="#"><img class="media-object" src="'+ book[i].images +'" alt="' + book[i].title + '"></a></div><div class="media-body"><span class="media-heading title" id="title">' + book[i].title + '</span> <div class="author">' + book[i].author + '</div><div class="summary">' + book[i].summary + '</div></div></div>';
+            books.innerHTML += mediaHtml;
+        }
+    }
+};
+xmlhttp.open("GET","./books.json",true);
+xmlhttp.send();
