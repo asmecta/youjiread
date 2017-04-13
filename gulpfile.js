398232f8bd8f15dba0276ce0e5754c0f0dd02f52
aSmecta <jiaoxiangpo2007@live.cn>
2017-04-14 00:58:55 +0800

    使用Gulp进行构建

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Administrator on 2017/4/13.
+ */
+var gulp = require('gulp'),
+    htmlmini = require('gulp-html-minify'),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    csso = require('gulp-csso'),
+    filter = require('gulp-filter'),
+    RevAll = require('gulp-rev-all'),
+    del = require('del');
+
+gulp.task('default', ['del'], function () {
+    var jsFilter = filter('**/*.js', {restore: true}),
+        cssFilter = filter('**/*.css', {restore: true}),
+        htmlFilter = filter(['**/*.html'], {restore: true});
+    gulp.src('./src/*')
+        .pipe(useref())                         // 解析html中的构建块
+        .pipe(jsFilter)                         // 过滤所有js
+        .pipe(uglify())                         // 压缩js
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)                        // 过滤所有css
+        .pipe(csso())                           // 压缩优化css
+        .pipe(cssFilter.restore)
+        .pipe(RevAll.revision({                 // 生成版本号
+            dontRenameFile: ['.html'],          // 不给 html 文件添加版本号
+            dontUpdateReference: ['.html']      // 不给文件里链接的html加版本号
+        }))
+        .pipe(htmlFilter)                       // 过滤所有html
+        .pipe(htmlmini())                       // 压缩html
+        .pipe(htmlFilter.restore)
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('del', function () {
+    del('./dist');
+});
+
+gulp.task('ugly',function () {
+    gulp.src('./src/app.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+})
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /**
+ * version:v0.0.4
  * Created by Administrator on 2017/3/26.
  */
 'use trict'
@@ -21,14 +22,55 @@ xmlhttp.onreadystatechange=function()
     {
         var book = eval ("(" + xmlhttp.responseText + ")");
         for (var i = 0; i<book.length;i++){
-            var mediaHtml ='<div class="media"><div class="media-left"><a href="#"><img class="media-object" src="'+ book[i].image +'" alt="' + book[i].title + '"></a></div><div class="media-body"><span class="media-heading title" id="title">' + book[i].title + '</span><span class="subtitle">' +book[i].subtitle + '</span> <div class="author">' + book[i].author + '</div><div class="summary">' + book[i].summary + '</div></div></div>';
+            var mediaHtml ='<div class="media" id="'+ book[i].isbn13 + '" onclick="addJs(this.id)"><div class="media-left"><a><img class="media-object" src="'+ book[i].image +'" alt="' + book[i].title + '"></a></div><div class="media-body"><span class="media-heading title">' + book[i].title + '</span><span class="subtitle">' +book[i].subtitle + '</span> <div class="author">' + book[i].author + '</div><div class="summary">' + book[i].summary.replace("\n","<br>") + '</div></div></div>';
             books.innerHTML += mediaHtml;
         }
     }
 };
-xmlhttp.open("GET","./books.json?v0.0.2",true);
+xmlhttp.open("GET","./books.json",true);
 xmlhttp.send();
 
-function showDetails(a) {
-
+function showMore(a,b) {
+    a.style.display="none";
+    document.getElementById(b).style.height = "inherit";
+}
+function getDetails(result) {
+    for (var i in result){
+        if(selector(i) && (typeof result[i] == Object || Array)) {
+            selector(i).innerHTML = result[i];
+        }else if(selector(i)){
+            selector(i).innerHTML = result[i].replace("\n","<br>");
+        }
+    }
+    selector("img").alt=result.title;
+    selector("img").src = result.images.large;
+    document.getElementById("bookDetail").style.display="block";
+    selector("dburl").href=result.alt;
+    document.getElementsByTagName("title")[0].innerHTML=result.title;
+}
+function showDatils(data) {
+    console.log(data);
+}
+function selector(id) {
+    var x =document.getElementById(id);
+    return x;
+}
+function addJs(id) {
+        var JSONP = document.createElement("script");
+        JSONP.type = "text/javascript";
+        JSONP.id = "id";
+        JSONP.src = "https://api.douban.com/v2/book/isbn/" + id + "?callback=getDetails";
+        document.getElementsByTagName("head")[0].appendChild(JSONP);
+        JSONP.onload  = function () {
+            if (this.readyState || !this.readyState === "loaded" || !this.readyState === "complete" ) {
+                alert('暂无详细信息');
+                script.onload = script.onreadystatechange = null;
+            }
+        }
+}
+function hiddenDetails() {
+    document.getElementsByTagName("title")[0].innerHTML="友寄读书";
+    var child = document.getElementById("id");
+    document.getElementsByTagName("head")[0].removeChild(child);
+    document.getElementById("bookDetail").style.display="none";
 }
