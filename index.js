9547c1a43564939102151c7d94fbe6e7a9fd4b42
aSmecta <jiaoxiangpo2007@live.cn>
2017-04-27 10:45:31 +0800

    自动获取 书籍列表

--- index.js
+++ index.js
@@ -5,26 +5,40 @@
 var request = require('request'),
     fs = require('fs');
 fs.writeFileSync('./src/books.json',"");
-var bookList = JSON.parse(fs.readFileSync('./book.json'));
+var bookList = JSON.parse(fs.readFileSync('./isbn.json'));
 var doubanDetails = [];
+var item = bookList.length;
 for (var i = 0; i < bookList.length; i++) {
     var url = "https://api.douban.com/v2/book/isbn/" + bookList[i];
-    // console.log(i);
     request(url, function (error, reponse, body) {
         if (!error && reponse.statusCode == 200) {
             var details = JSON.parse(body);
             var iDetail = {};
-            iDetail["title"] = details.title,
-                iDetail["summary"] = details.summary,
-                iDetail["author"] = details.author,
-                iDetail["isbn13"] = details.isbn13,
-                iDetail["subtitle"] = details.subtitle,
-                iDetail["image"] = details.image;
-            fs.appendFile('./src/books.json', "," + JSON.stringify(iDetail));
-            console.log(iDetail)
+            if(details.isbn13) {
+                iDetail["title"] = details.title,
+                    iDetail["summary"] = details.summary,
+                    iDetail["author"] = details.author,
+                    iDetail["isbn13"] = details.isbn13,
+                    iDetail["subtitle"] = details.subtitle,
+                    iDetail["image"] = details.image;
+                doubanDetails.push(iDetail);
+                item--;
+            }else {
+                console.log("isbn");
+            }
+            if(item == 0){
+                console.log(JSON.stringify(doubanDetails));
+                fs.appendFile('./src/books.json', JSON.stringify(doubanDetails),function (err) {
+                    if(err){
+                        console.log("WriteError：" + err);
+                    }
+                });
+            }
+
         }
         else {
             console.log(error);
+            item--;
         }
     });
 };
